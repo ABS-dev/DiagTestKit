117f4cf3e1aafbcb7c6235f761859d7562be8de7
Thomas Kent <thomas.kent@usda.gov>
2023-10-30 08:54:40 -0500

    Notes on how to reprogram DiagTestKit

--- notes/run and save.R
+++ notes/run and save.R
@@ -1,5 +1,6 @@
 devtools::load_all()
 
+t0 <- proc.time()[3]
 ex1 <- suppressWarnings(
   estimateSnSp(dat = DiagTestKit::data1,
                Sn.ref = data.frame(ref = c(0.90, 0)),
@@ -8,10 +9,18 @@ ex1 <- suppressWarnings(
                control = estimateSnSpControl(seed = 64725,
                                              rep.iter = FALSE))
 )
+print(proc.time()[3] - t0)
+ex1
+
+
+
 
 lst <- as.list(environment())
-save(lst, file = "notes/version3.6.3.optmr.rdata")
+save(lst, file = "notes/version4.1.3.optmr.rdata")
+
 lst
 ex1
 
 S
+
+estimateSnSpControl(64725, rep.iter = FALSE)
